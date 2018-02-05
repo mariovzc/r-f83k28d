c9baf950b22002c88e5cc740ce0433b98412490c
Mario vizcaino <mario.vizcainos@gmail.com>
2018-02-05 16:50:32 -0500

    added increase value function

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      counter: 0
+    }
+  }
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.counter}</span>
+        <button id="inc" onClick={this.incrementVal.bind(this)}>Incrementa</button>
       </div>
     );
+  }
+  incrementVal () {
+    this.setState({
+      counter: this.state.counter + 1
+    })
   }
 }
 
